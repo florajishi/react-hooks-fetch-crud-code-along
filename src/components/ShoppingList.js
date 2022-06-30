7b33747477dc6019c6b885cebae1687f700d6f6f
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-06-29 21:43:13 -0500

    Completed

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -17,8 +17,24 @@ function ShoppingList() {
     setSelectedCategory(category);
   }
 
+  function handleUpdateItem(updatedItem){
+    const updatedItems = items.map((item) => {
+      if(item.id === updatedItem.id){
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems)
+  }
+
   function handleAddItem(newItem){
     setItems([...items], newItem)
+  }
+
+  function handleDeleteItem(deletedItem){
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems)
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -36,7 +52,10 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item}  
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
